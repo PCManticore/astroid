9d2e350f113f023d43a71d9a43aa5856cedc7b50
Claudiu Popa <pcmanticore@gmail.com>
2016-04-19 13:35:39 +0300

    Clean up the code a bit
    Use private functions instead of public ones and split into multiple lines
    the places where more than one operation was happening.

--- astroid/tree/zipper.py
+++ astroid/tree/zipper.py
@@ -23,14 +23,15 @@ from astroid.tree import node_classes
 # lists made with two-tuples.  The empty tuple is used to denote the
 # end of a linked list.  The zipper needs singly-linked lists for most
 # of its operations to take constant time.
-def linked_list(*values):
+def _linked_list(*values):
     '''Builds a new linked list of tuples out of its arguments.'''
     tail = ()
     for value in reversed(values):
         tail = (value, tail)
     return tail
 
-def reverse(linked_list):
+
+def _reverse(linked_list):
     '''Reverses an existing linked list of tuples.'''
     if linked_list:
         result = collections.deque((linked_list[0],))
@@ -43,18 +44,17 @@ def reverse(linked_list):
             tail = (result.pop(), tail)
         return tail
 
-def iterate(linked_list):
+
+def _iterate(linked_list):
     '''Return an iterator over a linked list of tuples.'''
     node = linked_list
     while node:
         yield node[0]
         node = node[1]
 
-def concatenate(left, right):
-    '''Takes two existing linked lists of tuples and concatenates the
-    first one onto the second.
 
-    '''
+def _concatenate(left, right):
+    '''Concatenate the first linked lists of tuples onto the second.'''
     if not left:
         return right
     elif not right:
@@ -70,14 +70,16 @@ def concatenate(left, right):
             tail = (result.pop(), tail)
         return tail
 
-def last(linked_list):
+
+def _last(linked_list):
     '''Returns the last element of a linked list of tuples.'''
     node = linked_list
     while node[1]:
         node = node[1]
     return node[0]
 
-def initial(linked_list):
+
+def _initial(linked_list):
     '''Returns a linked list of tuples containing all elements but the last.'''
     result = [linked_list[0]]
     tail = linked_list[1]
@@ -98,6 +100,7 @@ def initial(linked_list):
 #     changed (bool): Whether this zipper has been edited or not.
 Path = collections.namedtuple('Path', 'left right parent_nodes parent_path changed')
 
+
 class Zipper(wrapt.ObjectProxy):
     '''This an object-oriented version of a zipper with methods instead of
     functions.  All the methods return a new zipper or None, and none
@@ -113,6 +116,7 @@ class Zipper(wrapt.ObjectProxy):
             zipper's history.  This must be accessed as ._self_path.
 
     '''
+
     __slots__ = ('path')
 
     # Setting wrapt.ObjectProxy.__init__ as a default value turns it
@@ -152,14 +156,16 @@ class Zipper(wrapt.ObjectProxy):
 
         This takes time linear in the number of left siblings.'''
         if self._self_path and self._self_path.left:
-            focus, siblings = last(self._self_path.left), initial(self._self_path.left) 
-            path = self._self_path._replace(left=(), right=concatenate(reverse(siblings), (self.__wrapped__, self._self_path.right)))
+            focus, siblings = _last(self._self_path.left), _initial(self._self_path.left)
+            right = _concatenate(_reverse(siblings), (self.__wrapped__, self._self_path.right))
+            path = self._self_path._replace(left=(), right=right)
             return type(self)(focus=focus, path=path)
 
     def right(self):
         '''Go to the next sibling that's directly to the right of the focus.
 
-        This takes constant time.'''
+        This takes constant time.
+        '''
         if self._self_path and self._self_path.right:
             focus, right = self._self_path.right
             path = self._self_path._replace(left=(self.__wrapped__,
@@ -170,28 +176,33 @@ class Zipper(wrapt.ObjectProxy):
     def rightmost(self):
         '''Go to the rightmost sibling of the focus.
 
-        This takes time linear in the number of right siblings.'''
+        This takes time linear in the number of right siblings.
+        '''
         if self._self_path and self._self_path.right:
-            siblings, focus = initial(self._self_path.right), last(self._self_path.right)
-            path = self._self_path._replace(left=concatenate(reverse(siblings), (self.__wrapped__, self._self_path.left)),
-                                            right=())
+            siblings, focus = _initial(self._self_path.right), _last(self._self_path.right)
+            left = _concatenate(_reverse(siblings), (self.__wrapped__, self._self_path.left))
+            path = self._self_path._replace(left=left, right=())
             return type(self)(focus=focus, path=path)
 
     def down(self):
         '''Go to the leftmost child of the focus.
 
-        This takes constant time.'''
+        This takes constant time.
+        '''
         try:
             children = iter(self.__wrapped__)
             first = next(children)
         except StopIteration:
             return
-        path = Path(
-            left=(),
-            right=linked_list(*children),
-            parent_nodes=(self.__wrapped__, self._self_path.parent_nodes) if self._self_path else (self.__wrapped__, ()),
-            parent_path=self._self_path,
-            changed=False)
+        if self._self_path:
+            parent_nodes = (self.__wrapped__, self._self_path.parent_nodes)
+        else:
+            parent_nodes = (self.__wrapped__, ())
+        path = Path(left=(),
+                    right=_linked_list(*children),
+                    parent_nodes=parent_nodes,
+                    parent_path=self._self_path,
+                    changed=False)
         return type(self)(focus=first, path=path)
 
     def up(self):
@@ -200,7 +211,6 @@ class Zipper(wrapt.ObjectProxy):
         This takes time linear in the number of left siblings if the
         focus has been edited or constant time if it hasn't been
         edited.
-
         '''
         if self._self_path:
             left, right, parent_nodes, parent_path, changed = self._self_path
@@ -209,8 +219,9 @@ class Zipper(wrapt.ObjectProxy):
                 # This conditional uses parent_nodes to make going up
                 # take constant time if the focus hasn't been edited.
                 if changed:
+                    focus_node = _concatenate(_reverse(left), (self.__wrapped__, right))
                     return type(self)(
-                        focus=focus.make_node(concatenate(reverse(left), (self.__wrapped__, right))),
+                        focus=focus.make_node(focus_node),
                         path=parent_path and parent_path._replace(changed=True))
                 else:
                     return type(self)(focus=focus, path=parent_path)
@@ -218,7 +229,8 @@ class Zipper(wrapt.ObjectProxy):
     def root(self):
         '''Go to the root of the AST for the focus.
 
-        This takes time linear in the number of ancestors of the focus.'''
+        This takes time linear in the number of ancestors of the focus.
+        '''
         location = self
         while location._self_path:
             location = location.up()
@@ -235,15 +247,15 @@ class Zipper(wrapt.ObjectProxy):
 
         '''
         if self._self_path:
-            self_ancestors = reverse((self.__wrapped__, self._self_path.parent_nodes))
+            self_ancestors = _reverse((self.__wrapped__, self._self_path.parent_nodes))
         else:
             self_ancestors = (self.__wrapped__, ())
         if other._self_path:
-            other_ancestors = reverse((other.__wrapped__, other._self_path.parent_nodes))
+            other_ancestors = _reverse((other.__wrapped__, other._self_path.parent_nodes))
         else:
             other_ancestors = (other.__wrapped__, ())
         ancestor = None
-        for self_ancestor, other_ancestor in zip(iterate(self_ancestors), iterate(other_ancestors)):
+        for self_ancestor, other_ancestor in zip(_iterate(self_ancestors), _iterate(other_ancestors)):
             # This is a kludge to work around the problem of two Empty
             # nodes in different parts of an AST.  Empty nodes can
             # never be ancestors, so they can be safely skipped.
@@ -347,17 +359,15 @@ class Zipper(wrapt.ObjectProxy):
                 the focus with.
         '''
         return type(self)(focus=focus, path=self._self_path._replace(changed=True))
-    
+
     # def edit(self, *args, **kws):
     #     return type(self)(focus=self.__wrapped__.make_focus(*args, **kws),
     #                       path=self._self_path._replace(changed=True))
 
-    
     # Legacy APIs
     @property
     def parent(self):
-        '''Goes up to the next ancestor of the focus that's a node, not a
-        sequence.'''
+        '''Goes up to the next ancestor of the focus that's a node, not a sequence.'''
         location = self.up()
         if isinstance(location, collections.Sequence):
             return location.up()
@@ -365,10 +375,7 @@ class Zipper(wrapt.ObjectProxy):
             return location
 
     def get_children(self):
-        '''Iterates over nodes that are children or grandchildren, no
-        sequences.
-
-        '''
+        '''Iterates over nodes that are children or grandchildren, no sequences.'''
         child = self.down()
         while child is not None:
             if isinstance(child, collections.Sequence):
@@ -436,9 +443,10 @@ class Zipper(wrapt.ObjectProxy):
     def frame(self):
         '''Go to the first ancestor of the focus that creates a new frame.
 
-        This takes time linear in the number of ancestors of the focus.'''
+        This takes time linear in the number of ancestors of the focus.
+        '''
         location = self
-        while (location is not None and 
+        while (location is not None and
                not isinstance(location.__wrapped__,
                               (node_classes.FunctionDef, node_classes.Lambda,
                                node_classes.ClassDef, node_classes.Module))):
@@ -452,16 +460,16 @@ class Zipper(wrapt.ObjectProxy):
         ClassDef, Lambda, GeneratorExp, and comprehension nodes.  On
         Python 2, the same is true except that list comprehensions
         don't generate a new scope.
-
         """
         return scope.node_scope(self)
 
     def statement(self):
         '''Go to the first ancestor of the focus that's a Statement.
 
-        This takes time linear in the number of ancestors of the focus.'''
+        This takes time linear in the number of ancestors of the focus.
+        '''
         location = self
-        while (location is not None and 
+        while (location is not None and
                not isinstance(location.__wrapped__,
                               (node_classes.Module, node_classes.Statement))):
             location = location.up()
